47fa98897951786f6e5b18228de06ec2aef202c8
maria_cvr07@hotmail.com <maria_cvr07@hotmail.com>
2026-03-19 10:06:04 +0000

    Change in project to make every mart result a table

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,4 +1,3 @@
-{{config(materialized='table')}}
 SELECT date_date, COUNT(orders_id) AS nb_transactions, SUM(revenue_per_order) AS total_revenue, AVG(revenue_per_order) AS average_basket,
  sum(operational_margin) AS daily_operational_margin, SUM(total_cost_per_order) AS total_purchase_cost, SUM(shipping_fee) AS total_shipping_fees,
  SUM(logcost) AS total_logcost, SUM(nb_products) AS nb_products_sold
